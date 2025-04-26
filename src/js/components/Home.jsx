d6e49b9e36122a1b9250205a17cc23c2cab15ddb
23Ousama <Elyazidi2001@gmail.com>
2025-04-26 15:18:46 +0000

    Ejercicio realizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,61 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Contador({ seconds }) {
+  const one = seconds % 10;
+  const two = Math.floor(seconds / 10) % 10;
+  const three = Math.floor(seconds / 100) % 10;
+  const four = Math.floor(seconds / 1000) % 10;
+  const five = Math.floor(seconds / 10000) % 10;
+  const six = Math.floor(seconds / 100000) % 10;
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+  return (
+    <div style={{
+      textAlign: "center",
+      backgroundColor: "#090909",
+      padding: "20px",
+      margin: "20px",
+      borderRadius: "10px"
+    }}>
+      <div style={{
+        display: "inline-block",
+        background: "black",
+        color: "white",
+        width: "80px",
+        height: "80px",
+        lineHeight: "80px",
+        margin: "5px",
+        border: "2px solid white",
+        borderRadius: "10px",
+        fontSize: "30px"
+      }}>
+        <FontAwesomeIcon icon={faClock} />
+      </div>
+      {[six, five, four, three, two, one].map((digit, index) => (
+        <div key={index} style={{
+          display: "inline-block",
+          background: "black",
+          color: "white",
+          width: "80px",
+          height: "80px",
+          lineHeight: "80px",
+          margin: "5px",
+          border: "2px solid white",
+          borderRadius: "10px",
+          fontSize: "30px"
+        }}>
+          {digit}
+        </div>
+      ))}
+    </div>
+  );
+}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+Contador.propTypes = {
+  seconds: PropTypes.number,
 };
 
-export default Home;
+export default Contador;
+
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Contador from './components/Home';
+let seconds = 0;
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-//Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+setInterval(() => {
+  const one = seconds % 10;
+  const two = Math.floor(seconds / 10) % 10;
+  const three = Math.floor(seconds / 100) % 10;
+  const four = Math.floor(seconds / 1000) % 10;
+  const five = Math.floor(seconds / 10000) % 10;
+  const six = Math.floor(seconds / 100000) % 10;
+  root.render(
+    <Contador seconds={seconds} /> 
+  );
 
-// index.css'
-import '../styles/index.css'
-
-// components
-import Home from './components/Home';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+  seconds++;
+}, 1000);
